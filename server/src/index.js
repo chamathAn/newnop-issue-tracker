2b1e71f0a39c091dbfd0dc09aa7bc365df98ab55
Chamath Anjula <124298134+chamathAn@users.noreply.github.com>
2026-05-22 18:41:54 +0530

    build: update frontend url

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 const corsOrigins =
   process.env.NODE_ENV === 'production'
-    ? ['newnop-issue-tracker-puce.vercel.app']
+    ? ['newnop-issue-tracker-g797mogtn-chamath-anjulas-projects.vercel.app']
     : ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:8000'];
 app.use(cors({ origin: corsOrigins, credentials: true }));
 app.use(express.json());
