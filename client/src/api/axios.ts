13bf502eb63baf7cf8d516591d991022fc27595a
Chamath Anjula <124298134+chamathAn@users.noreply.github.com>
2026-05-22 18:44:34 +0530

    build: update backend url

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://newnop-issue-tracker.onrender.com//api',
+  baseURL: 'https://newnop-issue-tracker.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
